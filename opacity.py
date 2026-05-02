changed nabla_ad calculation to correct formulation in textbook

opacity.py
@@ -14,7 +14,7 @@ def interp_opacity(log_rho, logT, interpolator):
     logT_min, logT_max = interpolator.grid[0].min(), interpolator.grid[0].max()
     logR_min, logR_max = interpolator.grid[1].min(), interpolator.grid[1].max()
 
-    if (logT < logT_min) or (logT > logT_max) or np.isnan(logT):
+    if np.any(logT < logT_min) or np.any(logT > logT_max) or np.isnan(logT):
         print(f"logT = {logT} value is outside range for provided table.")
         return 1e10
 
@@ -25,7 +25,7 @@ def interp_opacity(log_rho, logT, interpolator):
     
     logR = np.log10(R)
 
-    if (logR < logR_min) or (logR > logR_max) or np.isnan(logR):
+    if np.any(logR < logR_min) or np.any(logR > logR_max) or np.isnan(logR):
         print(f"logR = {logR} value is oustide range for provided table.")
         return 1e10
 

stellar_model.py
@@ -143,9 +143,29 @@ class StellarStructure:
         P_array = soln[1, :]
         T_array = soln[2, :]
         R_array = soln[3, :]
-
+        
         rho_array = calculate_rho(P_array, T_array, self.mu)
+        
+        # Calculate nabla values with the final stellar properties. This will enable us to determine which parts of the star are convective or radiative. 
+        kappa_array = []
+        e_pp_array = []
+        e_cno_array = []
+        X, Y = self.composition
+        for T, rho in zip(T_array, rho_array):
+            kappa = interp_opacity(np.log10(rho), np.log10(T), self.interpolator)
+            kappa_array.append(kappa)
+            
+            e_pp, e_cno = calculate_energies(rho, T, X, Y)
+            e_pp_array.append(e_pp)
+            e_cno_array.append(e_cno)
+        
+        #kappa_array = interp_opacity(np.log10(rho_array), np.log10(T_array), self.interpolator)
+        
+        nabla_array, convective_bool = calculate_nabla(T_array, masses, L_array, P_array, kappa_array)
+        
+        # Calculate energy-generation rates for different temperatures in the star.
 
-        return L_array, P_array, T_array, R_array, rho_array, masses
+
+        return L_array, P_array, T_array, R_array, rho_array, masses, kappa_array, nabla_array, convective_bool, (e_pp_array, e_cno_array)
         
     

utils.py
@@ -73,6 +73,14 @@ def P_equations(rho_values, T, M, Rstar, mu, interpolator):
 
     return 1-(P1/P2)
 
+def calc_g(M, R):
+    G = 6.674*10**(-8)
+    return GM/R**2
+
+def calc_Teff(L, R):
+    sigma = 5.67*10**(-5)
+    return (L/(4*np.pi*sigma))**(1/4)
+
 def derivs(M_r, y, composition, interpolator):
 
     K = 1.38*10**(-16)
@@ -99,8 +107,9 @@ def derivs(M_r, y, composition, interpolator):
     kappa = interp_opacity(np.log10(rho), np.log10(T), interpolator)
 
     nabla_rad = 3/(16*np.pi*a*c) * P * kappa/T**4 * L/(G * M_r)
-    nabla_ad = 0.4
-    if nabla_rad < nabla_ad:
+    beta = 1-(a*T**4)/(3*P)
+    nabla_ad = (2*(4-3*beta))/(32-24*beta-3beta**2)
+    if nabla_rad <= nabla_ad:
         nabla = nabla_rad
     else:
         nabla = nabla_ad
@@ -129,6 +138,32 @@ def calculate_rho(P, T, mu):
     rho = (P - 1/3 * a * T**4)*mu/(N_a * K * T)
     return rho
 
+def calculate_nabla(T, M_r, Lr, Pr, kappa):
+    K = 1.38*10**(-16)
+    N_a = 6.0221408 * 10**(23)
+
+    a = 7.56557705*10**(-15)
+    c = 2.998*10**(10)
+    G = 6.674*10**(-8)
+    
+    nabla_rad = 3/(16*np.pi*a*c) * Pr * kappa/T**4 * Lr/(G * M_r)
+    beta = 1-(a*T**4)/(3*Pr)
+    nabla_ad = (2*(4-3*beta))/(32-24*beta-3beta**2)
+    nabla = 0.0
+    convective = False
+    
+    nabla = np.where(nabla_rad < nabla_ad, nabla_rad, nabla_ad)
+    convective = nabla_rad <= nabla_ad
+    #if nabla_rad < nabla_ad:
+    #    nabla = nabla_rad
+    #    convective = False
+    #else:
+    #    nabla = nabla_ad
+    #    convective = True
+        
+    return nabla, convective
+    
+    
 def load1(M_r, composition, Pc, Tc, interpolator):
     # Gives the value of T, L, r, and P just slightly off from the center of the star
     #print("got here")
@@ -144,8 +179,6 @@ def load1(M_r, composition, Pc, Tc, interpolator):
     
     X, Y = composition
     mu = 4/(3+5*X)
-
-    nabla_adc = 0.4
 
     # Equation of state
     rho_c = calculate_rho(Pc, Tc, mu)
@@ -173,7 +206,8 @@ def load1(M_r, composition, Pc, Tc, interpolator):
 
     #Determine if we are in a convective or radiative layer
     nabla_rad = 3/(16*np.pi*a*c) * Pr * kappa_c/Tr_rad**4 * Lr/(G * M_r)
-    nabla_ad = 0.4
+    beta = 1-(a*Tc**4)/(3*Pc)
+    nabla_adc = (2*(4-3*beta))/(32-24*beta-3beta**2)
     if nabla_rad < nabla_ad:
         Tr = Tr_rad
         nabla = nabla_rad